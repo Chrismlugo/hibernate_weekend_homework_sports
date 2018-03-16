050d73fd81c022016a6944563b20a1c5441fc7f2
Chris Murphy <_chrism_@live.com>
2018-03-16 10:56:54 +0000

    created Employee superclass. added properties and annotations

---
+++ src/main/java/db/DBHelper.java
@@ -0,0 +1,9 @@
+package db;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class DBHelper {
+    private Transaction transaction;
+    private Session session;
+}
---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,47 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Employee {
+    private int id;
+    private String name;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee( String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name="salary")
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
